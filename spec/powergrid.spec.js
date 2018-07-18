2c5b6566f8240388c0f3ee878a4be03a0370cc0b
Wasim Pathan <wasim.a.pathan@zsassociates.com>
2018-07-18 14:38:26 +0530

    Test: Fixing the build

--- spec/powergrid.spec.js
+++ spec/powergrid.spec.js
@@ -50,7 +50,7 @@ describe('powergrid', function () {
 	it('can position grid cells', function () {
 		expect(typeof powergrid.gridCell).toBe('function');
 		var props = powergrid.gridCell(1);
-		expect(props.gridColumnStart).toBe(0);
+		expect(props.gridColumnStart).toBe(1);
 		expect(props['-ms-grid-column']).toBe(1);
 		//TODO: add more cases
 	});
